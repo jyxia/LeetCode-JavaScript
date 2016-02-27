559615ed9146dcfb91d1f8303a6f72534b480545
Jinyue Xia <xiajinyue@gmail.com>
2016-02-27 18:17:46 -0500

    just add two: 116, 230

---
+++ Medium/116-PopulatingNextRightPointersinEachNode.js
@@ -0,0 +1,29 @@
+/**
+ * Definition for binary tree with next pointer.
+ * function TreeLinkNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = this.next = null;
+ * }
+ */
+
+/**
+ * Key: if a node is a parent's left child, then this node's next is its parent's right child node.
+ * if a node is a parent node's right child, then this node's next is its parent's next node's left node.
+ * (the parent node should have next child). The trick part is how to write code. 
+ *
+ * @param {TreeLinkNode} root
+ * @return {void} Do not return anything, modify tree in-place instead.
+ */
+var connect = function(root) {
+    if (!root) return;
+    while (root.left) {
+        // track every level, starts from the left
+        var levelP = root;
+        while (levelP) {
+            levelP.left.next = levelP.right;
+            if (levelP.next) levelP.right.next = levelP.next.left;
+            levelP = levelP.next;
+        }
+        root = root.left;
+    }
+};
---
+++ Medium/230-kthSmallestElementinBST.js
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * Key: just use inorderTraversal to traverse the tree. Inorder traversal
+ * travels the node in a ascending order because it is a BST...
+ *
+ * @param {TreeNode} root
+ * @param {number} k
+ * @return {number}
+ */
+var kthSmallest = function(root, k) {
+    var stack = [];
+    var node = root;
+
+    while (node || stack.length > 0) {
+        if (node) {
+            stack.push(node);
+            node = node.left;
+        } else {
+            node = stack.pop();
+            k--;
+            if (k === 0) return node.val;
+            node = node.right;
+        }
+    }
+};
