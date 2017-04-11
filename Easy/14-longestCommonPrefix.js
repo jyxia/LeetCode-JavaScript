ff2ce5f5337c9cf2b40853125849f85fda786f18
Jinyue Xia <xiajinyue@gmail.com>
2017-04-11 10:23:07 -0400

    Update 14-longestCommonPrefix.js
    use JavaScript reduce

--- Easy/14-longestCommonPrefix.js
+++ Easy/14-longestCommonPrefix.js
@@ -16,3 +16,18 @@ var longestCommonPrefix = function(strs) {
 
     return firstString.substring(0, commonLength);
 };
+
+// a better way
+var longestCommonPrefix = function(strs) {
+    if (strs.length === 0) {
+        return '';
+    }
+    
+    return strs.reduce(function(pre, str) {
+        while (str.indexOf(pre) !== 0) {
+            pre = pre.slice(0, pre.length - 1); 
+        }
+        
+        return pre;
+    }, strs[0]);
+};
